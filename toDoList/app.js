5e8b83d3dce2be344c1cc1f26c638b9408963d27
EleNdor <amo123beach@gmail.com>
2024-03-14 22:45:01 +0300

    появление ошибки

--- toDoList/app.js
+++ toDoList/app.js
@@ -10,56 +10,93 @@ const listElement1 = document.getElementById("firstContent")
 const listElement2 = document.getElementById("secondContent")
 const listElement3 = document.getElementById("thirdContent")
 const listElement4 = document.getElementById("fourthContent") 
-const arrayNoties = {
-    index: 1,
-    time: false,
-    importance:false,
-    noteText: 'test',
-}
-const span = document.getElementsByClassName('span')
-
+const arrayNoties = [{
+    index:0,
+    time: checkerTime.checked,
+    importance:checkerImportance.checked,
+    noteText: note.value,
+    matter:0,
+},]
+const aceepter = document.getElementById("donebtn")
+const allNoties = document.getElementById("allNoties")
 creater.onclick = function(){
     newNotes(note.value,checkerTime.checked,checkerImportance.checked)
-    note.value = ''
+    note.value = ""
     checkerImportance.checked = false 
-    checkerTime.checked = false
-    
+    checkerTime.checked = false 
 }
-console.log(checkerImportance.checked)
 
 
 
 function newNotes(note,time,importance){
+    console.log(note.value)
     if ((time == true) && (importance == true)){
+        if(note.value == undefined){
+            return
+        }
+        {
         listElement1.insertAdjacentHTML('beforeend',`
         <li>
             <span>${note}<span/>
         </li>
         `) 
-    }
+        allNoties.insertAdjacentHTML('beforeend',`
+        <li class="listinall"><button id="donebtn">☑</button>lorem<span></span></li>
+        `)
+        
+    }}
     if ((time == false) && (importance == true)){
+        if(note.value == undefined){
+            return
+        }
+        else{
         listElement2.insertAdjacentHTML('beforeend',`
         <li>
             <span>${note}<span/>
         </li>
         `) 
-    }
+
+    }}
     if ((time == true )&& (importance == false)){
+        if(note.value == undefined){
+            return
+        }
+        else{
         listElement3.insertAdjacentHTML('beforeend',`
         <li>
             <span>${note}<span/>
         </li>
         `) 
-    }
-    if((time == false )&& (importance == false)) {
+
+    }}
+    if((time == false )&& (importance == false)){
+        if(note.value == undefined){
+            return
+        }
+        else{
         listElement4.insertAdjacentHTML('beforeend',`
         <li>
             <span>${note}<span/>
         </li>
         `) 
-    }
+        allNoties.insertAdjacentHTML('beforeend',`
+        <li class="delete"><button id="donebtn">☑</button>lorem
+        
+        </li>
+        `)
+        console.log(note.value)
+    }}
 }
 
-span.onclick = function(){
-    console.log('2312')
-}
+ aceepter.onclick = function(){
+    const newNote = {
+        index:arrayNoties.length,
+        time: checkerTime.checked,
+        importance:checkerImportance.checked,
+        noteText: note.value,
+        matter:0     
+    }
+    arrayNoties.push(newNote)
+    console.log(newNote.noteText)
+    }
+
